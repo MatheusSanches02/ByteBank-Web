439f2230a64349de5100a4efff3ac23ebe24476c
MatheusSanches02 <ma12sanches@gmail.com>
2024-01-24 23:14:49 -0300

    novas páginas e implementação de testes de integração

--- bytebank/src/componentes/Menu/Menu.test.js
+++ bytebank/src/componentes/Menu/Menu.test.js
@@ -1,26 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import Menu from './index';
+import { BrowserRouter } from 'react-router-dom';
 
 test('Deve renderizar um link para a página inicial', () => {
-  render(<Menu />);
-  const linkPaginaInicial = screen.getByText('Inicial');
+  render(<Menu />, { wrapper: BrowserRouter });
+  const linkPaginaInicial = screen.getByText('Início');
   expect(linkPaginaInicial).toBeInTheDocument();
 });
 
 test('Deve renderizar uma lista de link', () => {
-  render(<Menu />);
+  render(<Menu />, { wrapper: BrowserRouter });
   const listaDeLink = screen.getAllByRole('link');
   expect(listaDeLink).toHaveLength(4);
 });
 
 test('Não deve renderizar o link para Extrato', () => {
-  render(<Menu />);
+  render(<Menu />, { wrapper: BrowserRouter });
   const linkExtrato = screen.queryByText('Extrato');
   expect(linkExtrato).not.toBeInTheDocument();
 });
 
 test('Deve renderizar uma lista de links com a classe link', () => {
-  render(<Menu />);
+  render(<Menu />, { wrapper: BrowserRouter });
   const links = screen.getAllByRole('link');
   links.forEach((link) => expect(link).toHaveClass('link'));
   expect(links).toMatchSnapshot();
---
+++ bytebank/src/routes.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import App from './paginas/Principal/App'
+import AppRoutes from './routes'
+import Cartoes from './componentes/Cartoes'
+import { BrowserRouter, MemoryRouter, Routes, Route } from "react-router-dom";
+
+describe('Rotas', () => {
+    test('Deve renderizar a página principal', () => {
+        render(<App />, {wrapper: BrowserRouter});
+        const usuario = screen.getByText('Olá, Joana :)!');
+        expect(usuario).toBeInTheDocument();
+    })
+    test('Deve renderizar a página de cartões', () => {
+        const rota = '/cartoes';
+        render(<MemoryRouter initialEntries={[rota]}>
+                <Routes>
+                    <Route path="/" element={<App />} >
+                        <Route path="/cartoes" element={<Cartoes />} />
+                    </Route>
+                </Routes>
+            </MemoryRouter>);
+
+        
+        const cartoes = screen.getByText('Meus cartões');
+        expect(cartoes).toHaveTextContent('Meus cartões');
+    })
+    test('Deve renderizar a localização da rota atual', () => {
+        const rota = '/cartoes';
+        render(<MemoryRouter initialEntries={[rota]}>
+                <App/>
+            </MemoryRouter>);
+
+        const localizacao = screen.getByTestId('local');
+
+        expect(localizacao).toHaveTextContent(rota);
+    })
+    test('Deve renderizar a página de not found', () => {
+        const rota = '/extrato';
+        render(<MemoryRouter initialEntries={[rota]}>
+                <AppRoutes/>
+        </MemoryRouter>)
+
+        const paginaErro = screen.getByTestId('pagina-404');
+        expect(paginaErro).toContainHTML('<h1>Ops! Não encontramos a página</h1>');
+    })
+})
